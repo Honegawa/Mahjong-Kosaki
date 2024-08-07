91423ad7b8eae96c261bc2245ac5e5437c437b4a
Honegawa <taing.steven@outlook.fr>
2024-08-07 11:52:03 +0200

    Add mailer for contact

--- backend/controllers/contact.controller.js
+++ backend/controllers/contact.controller.js
@@ -1,90 +1,33 @@
-import { Contact, Person } from "../models/index.js";
+import nodemailer from "nodemailer";
+import { env } from "../configs/config.js";
 
-export const getAll = async (req, res) => {
-  try {
-    const contacts = await Contact.findAll({
-      include: {
-        model: Person,
-        attributes: ["firstname", "lastname", "email", "phone"],
-      },
-    });
-
-    res.status(200).json(contacts);
-  } catch (error) {
-    res.status(500).json({ error: "Error in fetching contact" });
-  }
-};
-
-export const getById = async (req, res) => {
-  try {
-    const { id } = req.params;
-    const contact = await Contact.findByPk(id, {
-      include: {
-        model: Person,
-        attributes: ["firstname", "lastname", "email", "phone"],
-      },
-    });
-
-    if (!contact) {
-      return res.status(404).json({ message: "Contact not found" });
-    }
-
-    res.status(200).json(contact);
-  } catch (error) {
-    res.status(500).json({ error: "Error in fetching contact" });
-  }
-};
+const transport = nodemailer.createTransport({
+  host: env.MAIL_HOST,
+  port: env.MAIL_FROM,
+  auth: {
+    user: env.MAIL_USER,
+    pass: env.MAIL_PASSWORD,
+  },
+});
 
 export const create = async (req, res) => {
   try {
-    const { firstname, lastname, email, phone, object, content } = req.body;
+    const { firstname, lastname, email, object, content } = req.body;
 
-    const person = await Person.findOne({ where: { email: email } });
-    if (!person) {
-      const newPerson = await Person.create({
-        firstname,
-        lastname,
-        email,
-        phone,
-      });
-
-      const contact = await Contact.create({
-        object,
-        content,
-        PersonId: newPerson.id,
-      });
-      res.status(201).json({ message: "Contact has been created", contact });
-    } else {
-      const contact = await Contact.create({
-        object,
-        content,
-        PersonId: person.id,
-      });
-      res.status(201).json({ message: "Contact has been created", contact });
-    }
+    sendMailNotif(firstname, lastname, email, object, content).catch(
+      console.error
+    );
+    res.status(201).json({ message: "Message has been sent" });
   } catch (error) {
     res.status(500).json({ error: "Error in sending contact" });
   }
 };
 
-export const deleteById = async (req, res) => {
-  try {
-    const { id } = req.params;
-    const contact = await Contact.findByPk(id);
-
-    if (!contact) {
-      return res.status(404).json({ message: "Contact not found" });
-    }
-
-    if (req.user.role !== "admin") {
-      return res
-        .status(403)
-        .json({ message: "Unauthorized to delete contact" });
-    }
-
-    await contact.destroy();
-    res.status(200).json({ message: "Contact has been deleted" });
-  } catch (error) {
-    res.status(500).json({ error: "Error in deleting contact" });
-  }
+const sendMailNotif = async (firstname, lastname, email, object, content) => {
+  const info = await transport.sendMail({
+    from: `"${firstname} ${lastname}" <${email}>`,
+    to: "asso.kosaki@gmail.com",
+    subject: object,
+    text: content,
+  });
 };
